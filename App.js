40c6e575d7c981a509f40fc8ee45565d6db3c34e
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-24 07:11:11 -0700

    Update welcome message in App component

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hệ Thống Quản Lý Đất Nền</h1>
-      <p>Chào mừng bạn đến với hệ thống quản lý đất nền của chúng tôi!</p>
+      <p>Chào mừng bạn đến với hệ thống quản lý đất nền của chúng tôi. Chúng tôi hy vọng bạn có trải nghiệm tốt nhất!</p>
     </div>
   );
 }
