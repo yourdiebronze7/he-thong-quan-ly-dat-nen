f6e2012f9cafcc679a6c6e206507052ad586da37
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-24 07:12:31 -0700

    Updated response message for the root endpoint.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Land management system is operational!');
+    res.send('Welcome to the Land Management System!');
 });
 
 app.listen(PORT, () => {
