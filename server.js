9db2c9c3763f6de9f4421104ae00db20c2018aba
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-24 07:11:40 -0700

    Update welcome message to English for broader audience.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hệ thống quản lý đất nền hoạt động!');
+    res.send('Land management system is operational!');
 });
 
 app.listen(PORT, () => {
