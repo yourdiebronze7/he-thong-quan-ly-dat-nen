22229fcbe657d45d48f4dbb607e37a1ecadec6a2
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-24 07:09:08 -0700

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hệ Thống Quản Lý Đất Nền</h1>
+      <p>Chào mừng bạn đến với hệ thống quản lý đất nền của chúng tôi!</p>
+    </div>
+  );
+}
+
+export default App;
